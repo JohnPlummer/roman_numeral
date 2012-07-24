8eb34765ecff53fbbccefbde63fea153a552e4c7
John Plummer <john@memphis.co.uk>
2012-07-24 14:30:37 +0100

    return 1 for I

---
+++ roman_numeral.rb
@@ -0,0 +1,7 @@
+class RomanNumeral
+
+  def to_integer
+     return 1
+  end
+
+end
--- roman_numeral_spec.rb
+++ roman_numeral_spec.rb
@@ -3,3 +3,13 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 require 'rspec'
 require 'roman_numeral'
 
+describe RomanNumeral do
+
+  describe 'to_integer' do
+
+    it 'returns the value 1 for symbol I' do
+      RomanNumeral.new('I').to_integer.should == 1
+    end
+
+  end
+end
